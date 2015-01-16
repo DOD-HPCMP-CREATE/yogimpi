c4ba6fa4583622d58dfd85bfb64e311d0fa5091d
Brian Pittman <brian.pittman@arnold.af.mil>
2015-01-16 08:05:51 -1000

    Extend wrapping code to support more MPI datatypes (still more work to be done, tho)

--- GenerateWrap.py
+++ GenerateWrap.py
@@ -30,6 +30,7 @@ class GenerateWrap:
                                 #so the nativeCallList would list mpi_comm instead of comm
 
          setupLines = '' #lines of code that precede the native MPI call
+         teardownLines = '' #lines of code that follow the native MPI call
 
          #each child of the current <Function> element is either an <Arg> or a <ReturnType>
          for grandchild in child:
@@ -44,6 +45,7 @@ class GenerateWrap:
                argName = grandchild.attrib['name']
                argType = grandchild.attrib['type']
                argInput = True if 'input' in grandchild.attrib and grandchild.attrib['input'] == "true" else False
+               argOutput = True if 'output' in grandchild.attrib and grandchild.attrib['output'] == "true" else False
                typedArgList += argType + ' ' + argName
                argList += argName 
 
@@ -56,9 +58,79 @@ class GenerateWrap:
                elif argType == 'MPI_Comm' and argInput:
                   setupLines += '   MPI_Comm mpi_'+argName+' = comm_to_mpi('+argName+');\n'
                   nativeCallArgList += 'mpi_'+argName 
+               elif argType == 'MPI_Comm*' and argOutput:
+                  pass
+                  #FIXME: MPI_Comms as outputs only appear in MPI_Comm_create and MPI_Comm_free, and
+                  #       each of these are handled differently, so handwrap?
 
-               #FIXME: support for group_to_mpi, comm_to_mpi, request_to_mpi, op_to_mpi, file_to_mpi, info_to_mpi,
-               #       and comparison_to_yogi. Also needs to support output parameters, and references/pointers are
+               #convert MPI_Status to native format before native MPI call
+               elif argType == 'MPI_Status*' and argInput:
+                  #FIXME: array of statuses in MPI_Waitall
+                  setupLines += '   MPI_Status *mpi_'+argName+' = yogi_status_to_mpi('+argName+');\n'
+                  nativeCallArgList += 'mpi_'+argName 
+
+               #convert MPI_Op to native format before native MPI call
+               elif argType == 'MPI_Op' and argInput:
+                  setupLines += '   MPI_Op mpi_'+argName+' = op_to_mpi('+argName+');\n'
+                  nativeCallArgList += 'mpi_'+argName 
+
+               #convert MPI_Request to/from native format
+               elif argType == 'MPI_Request*':
+                  if argInput:
+                     setupLines += '   MPI_Request* mpi_'+argName+' = request_to_mpi(*'+argName+');\n'
+                     nativeCallArgList += 'mpi_'+argName
+                     #FIXME: check for MPI_REQUEST_NULL as in MPI_Wait?
+                     #FIXME: array of requests in MPI_Waitall
+                  elif argOutput:
+                     setupLines += '   MPI_Request mpi_'+argName+';\n'
+                     nativeCallArgList += '&mpi_'+argName
+                     teardownLines += '   *'+argName+' = alloc_new_request(mpi_'+argName+');\n'
+
+               #convert MPI_File to/from native format
+               elif argType == 'MPI_File*':
+                  if argInput:
+                     setupLines += '   MPI_File mpi_'+argName+' = file_to_mpi(*'+argName+');\n'
+                     nativeCallArgList += '&mpi_'+argName
+                  elif argOutput:
+                     setupLines += '   MPI_File mpi_'+argName+';\n'
+                     nativeCallArgList += '&mpi_'+argName
+                     teardownLines += '   *'+argName+' = alloc_new_file(mpi_'+argName+');\n'
+                     #FIXME: MPI_File_close doesn't set pointers to NULL
+               elif argType == 'MPI_File' and argInput:
+                  setupLines += '   MPI_File mpi_'+argName+' = file_to_mpi('+argName+');\n'
+                  nativeCallArgList += 'mpi_'+argName
+
+               #convert MPI_File to/from native format
+               elif argType == 'MPI_Info' and argInput:
+                  setupLines += '   MPI_Info mpi_'+argName+' = info_to_mpi('+argName+');\n'
+                  nativeCallArgList += 'mpi_'+argName
+               elif argType == 'MPI_Info*' and argOutput:
+                  setupLines += '   MPI_Info mpi_'+argName+';\n'
+                  nativeCallArgList += '&mpi_'+argName
+                  teardownLines += '   *'+argName+' = alloc_new_info(mpi_'+argName+');\n'
+
+               #convert MPI_Group to/from native format
+               elif argType == 'MPI_Group' and argInput:
+                  setupLines += '   MPI_Group mpi_'+argName+' = group_to_mpi('+argName+');\n'
+                  nativeCallArgList += 'mpi_'+argName
+               elif argType == 'MPI_Group*' and argOutput:
+                  pass
+                  #FIXME: might need to handwrap the output group stuff?
+
+               #check for cases where source == MPI_ANY_SOURCE
+               elif argName == 'source' and argInput:
+                  setupLines += '   if (YogiMPI_ANY_SOURCE == source) source = MPI_ANY_SOURCE;\n'
+                  nativeCallArgList += argName 
+
+               #check for cases where tag == MPI_ANY_TAG
+               #FIXME: this doesnt seem to be applied consistently in Stephen's handwritten code
+               elif argName == 'tag' and argInput:
+                  setupLines += '   if (YogiMPI_ANY_TAG == tag) source = MPI_ANY_TAG;\n'
+                  nativeCallArgList += argName 
+
+               #FIXME: YogiMPI_STATUS_IGNORE
+
+               #FIXME: Needs to support output parameters, and references/pointers are
                #       probably not handled correctly everywhere yet. In short, this section needs a lot of work.
                else:
                   nativeCallArgList += argName 
@@ -79,8 +151,10 @@ class GenerateWrap:
          #write implementation
          cFile.write(wrappedFunc+'\n{\n')
 
+         #FIXME: should int mpi_err = YogiMPI_SUCCESS; precede the setupLines?
          cFile.write(setupLines)
          cFile.write('   int mpi_error = '+nativeCall+';\n\n')
+         cFile.write(teardownLines)
          cFile.write('   return error_to_yogi(mpi_error);\n')
          cFile.write('}\n\n')
 
