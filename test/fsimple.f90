ca46b8a4f548e6e55ab68e111cb5573920ce5ac9
Stephen Adamec <sadamec@uab.edu>
2015-07-22 23:08:10 -0500

    Add fsimple.f90 source for fsimple test

---
+++ test/fsimple.f90
@@ -0,0 +1,14 @@
+program fsimple
+  implicit none
+  include 'mpif.h'
+  integer :: ierror, rank
+  logical :: initialized
+  call MPI_Init(ierror) 
+  call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierror) 
+  call MPI_Initialized(initialized, ierror)
+  if (initialized .neqv. .true.) then
+    call exit(1) 
+  end if
+  call MPI_Finalize(ierror)
+
+end program 
