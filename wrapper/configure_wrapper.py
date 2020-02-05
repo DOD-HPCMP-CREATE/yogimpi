0dbed5655255eb2d65aad70c226712e6ddadfaf7
Stephen Adamec <sadamec@uab.edu>
2020-02-05 11:31:11 -1000

    Ensure that a leading _ only matches a Fortran MPI regex for KIND parameters.

--- wrapper/configure_wrapper.py
+++ wrapper/configure_wrapper.py
@@ -10,12 +10,20 @@ inHandle = open('YogiMPIWrapper.py.in', 'r')
 wrapperContents = inHandle.read()
 inHandle.close()
 
+data['Type_Constants'] = []
+
 for tag in tags:
     data[tag] = []
     tagElem = root.find(tag)
     subTag = tag[:-1]
     for aTag in tagElem.findall(subTag):
+       if tag == 'Constants':
+           if aTag.attrib.get('type', None) == 'yes':
+               data['Type_Constants'].append(aTag.attrib.get('name')) 
+               continue
        data[tag].append(aTag.attrib.get('name'))
+
+for tag in data.keys():
     varName = 'mpi' + tag
     subString = '@MPI_' + tag.upper() + '@'
     outString = varName + ' = [ '
